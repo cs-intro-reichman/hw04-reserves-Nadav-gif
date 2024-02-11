bb51d54705b8055c907ce9c5fabdbfa096ac9d3b
Nadav Meroz <154219703+Nadav-gif@users.noreply.github.com>
2024-02-11 14:02:46 +0200

    Update StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,84 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String new_str = "";
+        char c = ' ';
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == 'A' || string.charAt(i) == 'E' || string.charAt(i) == 'I'|| string.charAt(i) == 'O' || string.charAt(i) == 'U' || string.charAt(i) == ' ') {
+                c = string.charAt(i);
+                new_str = new_str + c;
+            } else if (string.charAt(i) >64 && string.charAt(i) < 91) {
+                c = string.charAt(i);
+                c += 32;
+                new_str = new_str + c;
+            } else if (string.charAt(i) == 'a' || string.charAt(i) == 'e' || string.charAt(i) == 'i'|| string.charAt(i) == 'o' || string.charAt(i) == 'u') {
+                c = string.charAt(i);
+                c -= 32;
+                new_str = new_str + c;
+            } else if (string.charAt(i) > 96 && string.charAt(i) < 123) {
+                c = string.charAt(i);
+                new_str = new_str + c;
+            }
+        }
+        return new_str;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String new_str = "";
+        String final_str = "";
+        int first_space = 0;
+        char c;
+        for (int i = 0; i < string.length(); i++) {
+            c = string.charAt(i);
+            if (c == ' ') {
+                first_space++;
+            }
+            if (first_space == 0) {
+                if (c > 64 && c < 91) {
+                    c += 32;
+                }
+                new_str = new_str + c;
+            } else {
+                if (string.charAt(i-1) == ' ' && (c > 96 && c < 123)) {
+                    System.out.println("imm in");
+                    c -= 32;
+                } else if (c > 64 && c < 91 && string.charAt(i-1) != ' ') {
+                    c += 32;
+                }
+                new_str = new_str + c;
+            }
+        }
+        for (int i = 0; i < new_str.length(); i++) {
+            if (new_str.charAt(i) != ' ') {
+                final_str = final_str + new_str.charAt(i);
+            }
+        }
+        return final_str;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int [] arr = new int [string.length()];
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 0;
+        }
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                arr[i] = 1;
+                count++;
+            }
+        }
+        int [] final_arr = new int [count];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                final_arr[(final_arr.length - count)] = i;
+                count--;
+            }
+        }
+        return final_arr;
     }
 }
